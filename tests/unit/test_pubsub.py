e2087db5d4a282bd7e9db92acb6f8408aa238600
Júlio Vaz <juliolopesvaz@gmail.com>
2019-02-11 12:24:55 -0200

    Add publish test and remove unused file

--- tests/unit/test_pubsub.py
+++ tests/unit/test_pubsub.py
@@ -2,6 +2,12 @@ from unittest import mock
 from unittest import TestCase
 from flask_interface_gcp_pubsub.pubsub import PubSub, Publisher
 
+MOCK_DATA = {
+    'my_data': {
+        'key': 'value'
+    }
+}
+
 
 class TestPubSub(TestCase):
     @mock.patch.object(Publisher, '__init__', return_value=None)
@@ -15,3 +21,14 @@ class TestPubSub(TestCase):
         first_client = PubSub('test')
         second_client = PubSub('test2')
         self.assertNotEquals(first_client, second_client)
+
+    def test_publish(self):
+        publisher = mock.MagicMock()
+        client = PubSub('test_publish', publisher)
+        client.publish(MOCK_DATA)
+        expected = [
+            mock.call.publish(
+                'test_publish', data=b'{"my_data": {"key": "value"}}'
+            )
+        ]
+        self.assertEqual(publisher.method_calls, expected)
